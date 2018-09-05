136429ac80c930e898a589da1f4f27402f66a4d2
Michel Vocks <michel.vocks@dbschenker.com>
2018-09-05 22:30:54 +0200

    Updated tutorial test pipeline. Not tested yet

--- main.go
+++ main.go
@@ -2,41 +2,57 @@ package main
 
 import (
 	"encoding/base64"
-	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
 	"strings"
 
 	sdk "github.com/gaia-pipeline/gosdk"
+	vaultapi "github.com/hashicorp/vault/api"
 	"k8s.io/api/core/v1"
 	"k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-var (
-	vaultAddress        = "http://vault:8200"
-	vaultToken          = "root-token"
-	kubeConfVaultPath   = "secret/data/kube-conf"
-	appVersionVaultPath = "secret/data/nginx"
-	kubeLocalPath       = "/tmp/kube-conf"
-	appVersionLocalPath = "/tmp/app-version"
-	hostDNSName         = "host.docker.internal"
+const (
+	kubeConfVaultPath = "secret/data/kube-conf"
+	kubeLocalPath     = "/tmp/kubeconfig"
+)
 
-	// Deployment specific attributes
-	appName        = "nginx"
-	replicas int32 = 2
+var hostDNSName = "host.docker.internal"
+
+// Variables dynamically set during runtime.
+var (
+	vaultAddress string
+	vaultToken   string
+	imageName    string
+	replicas     int32
+	appName      string
+	clientSet    *kubernetes.Clientset
 )
 
 // GetSecretsFromVault retrieves all information and credentials
 // from vault and saves it to local space.
-func GetSecretsFromVault() error {
-	// Create vault client
-	vaultClient, err := connectToVault()
+func GetSecretsFromVault(args sdk.Arguments) error {
+	// Create new Vault client instance
+	vaultClient, err := vaultapi.NewClient(vaultapi.DefaultConfig())
 	if err != nil {
 		log.Printf("Error: %s\n", err.Error())
 		return err
 	}
+
+	// Set vault address
+	err = vaultClient.SetAddress(vaultAddress)
+	if err != nil {
+		log.Printf("Error: %s\n", err.Error())
+		return err
+	}
+
+	// Set token
+	vaultClient.SetToken(vaultToken)
 
 	// Read kube config from vault and decode base64
 	l := vaultClient.Logical()
@@ -60,38 +76,62 @@ func GetSecretsFromVault() error {
 	confStr = strings.Replace(confStr, "localhost", hostDNSName, 1)
 	kubeConf = []byte(confStr)
 
-	// Write kube config to file
-	if err = writeToFile(kubeLocalPath, kubeConf); err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return err
-	}
-
-	// Read app image version from vault
-	v, err := l.Read(appVersionVaultPath)
+	// Create file
+	f, err := os.Create(kubeLocalPath)
 	if err != nil {
 		log.Printf("Error: %s\n", err.Error())
 		return err
 	}
+	defer f.Close()
 
-	// Write image version to file
-	version := (v.Data["data"].(map[string]interface{}))["version"].(string)
-	if err = writeToFile(appVersionLocalPath, []byte(version)); err != nil {
-		log.Printf("Error: %s\n", err.Error())
+	// Write to file
+	_, err = f.Write(kubeConf)
+
+	log.Println("All data has been retrieved from vault!")
+	return nil
+}
+
+// PrepareDeployment prepares the deployment by setting up
+// the kubernetes client and caching all manual input from user.
+func PrepareDeployment(args sdk.Arguments) error {
+	// Setup kubernetes client
+	config, err := clientcmd.BuildConfigFromFlags("", kubeLocalPath)
+	if err != nil {
 		return err
 	}
-	log.Println("All data has been retrieved from vault!")
+
+	clientSet, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	// Cache given arguments for other jobs
+	for _, arg := range args {
+		switch arg.Key {
+		case "vault-address":
+			vaultAddress = arg.Value
+		case "vault-token":
+			vaultToken = arg.Value
+		case "image-name":
+			imageName = arg.Value
+		case "replicas":
+			rep, err := strconv.ParseInt(arg.Value, 10, 64)
+			if err != nil {
+				log.Printf("Error: %s\n", err)
+				return err
+			}
+			replicas = int32(rep)
+		case "app-name":
+			appName = arg.Value
+		}
+	}
+
 	return nil
 }
 
 // CreateNamespace creates the namespace for our app.
 // If the namespace already exists nothing will happen.
-func CreateNamespace() error {
-	// Get kubernetes client
-	c, err := getKubeClient(kubeLocalPath)
-	if err != nil {
-		return err
-	}
-
+func CreateNamespace(args sdk.Arguments) error {
 	// Create namespace object
 	ns := &v1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
@@ -100,8 +140,8 @@ func CreateNamespace() error {
 	}
 
 	// Lookup if namespace already exists
-	nsClient := c.Core().Namespaces()
-	_, err = nsClient.Get(appName, metav1.GetOptions{})
+	nsClient := clientSet.Core().Namespaces()
+	_, err := nsClient.Get(appName, metav1.GetOptions{})
 
 	// namespace exists
 	if err == nil {
@@ -110,7 +150,7 @@ func CreateNamespace() error {
 	}
 
 	// Create namespace
-	_, err = c.Core().Namespaces().Create(ns)
+	_, err = clientSet.Core().Namespaces().Create(ns)
 	if err != nil {
 		return err
 	}
@@ -121,21 +161,7 @@ func CreateNamespace() error {
 
 // CreateDeployment creates the kubernetes deployment.
 // If it already exists, it will be updated.
-func CreateDeployment() error {
-	// Get kubernetes client
-	c, err := getKubeClient(kubeLocalPath)
-	if err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return err
-	}
-
-	// Load image version from file
-	v, err := ioutil.ReadFile(appVersionLocalPath)
-	if err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return err
-	}
-
+func CreateDeployment(args sdk.Arguments) error {
 	// Create deployment object
 	d := &v1beta1.Deployment{}
 	d.ObjectMeta = metav1.ObjectMeta{
@@ -157,7 +183,7 @@ func CreateDeployment() error {
 				Containers: []v1.Container{
 					v1.Container{
 						Name:            appName,
-						Image:           fmt.Sprintf("%s:%s", appName, v),
+						Image:           imageName,
 						ImagePullPolicy: v1.PullAlways,
 						Ports: []v1.ContainerPort{
 							v1.ContainerPort{
@@ -171,8 +197,8 @@ func CreateDeployment() error {
 	}
 
 	// Lookup existing deployments
-	deployClient := c.ExtensionsV1beta1().Deployments(appName)
-	_, err = deployClient.Get(appName, metav1.GetOptions{})
+	deployClient := clientSet.ExtensionsV1beta1().Deployments(appName)
+	_, err := deployClient.Get(appName, metav1.GetOptions{})
 
 	// Deployment already exists
 	if err == nil {
@@ -197,14 +223,7 @@ func CreateDeployment() error {
 
 // CreateService creates the service for our application.
 // If the service already exists, it will be updated.
-func CreateService() error {
-	// Get kubernetes client
-	c, err := getKubeClient(kubeLocalPath)
-	if err != nil {
-		log.Printf("Error: %s\n", err.Error())
-		return err
-	}
-
+func CreateService(args sdk.Arguments) error {
 	// Create service obj
 	s := &v1.Service{
 		ObjectMeta: metav1.ObjectMeta{
@@ -226,7 +245,7 @@ func CreateService() error {
 	}
 
 	// Lookup for existing service
-	serviceClient := c.Core().Services(appName)
+	serviceClient := clientSet.Core().Services(appName)
 	currService, err := serviceClient.Get(appName, metav1.GetOptions{})
 
 	// Service already exists
@@ -258,25 +277,58 @@ func main() {
 			Handler:     GetSecretsFromVault,
 			Title:       "Get secrets",
 			Description: "Get secrets from vault",
-			Priority:    0,
+		},
+		sdk.Job{
+			Handler:     PrepareDeployment,
+			Title:       "Prepare Deployment",
+			Description: "Prepares the deployment (caches manual input and prepares kubernetes connection)",
+			DependsOn:   []string{"Get secrets"},
+			Args: sdk.Arguments{
+				sdk.Argument{
+					Type: sdk.VaultInp,
+					Key:  "vault-token",
+				},
+				sdk.Argument{
+					Type: sdk.VaultInp,
+					Key:  "vault-address",
+				},
+				sdk.Argument{
+					Type:        sdk.TextFieldInp,
+					Description: "Application name:",
+					Key:         "app-name",
+					Value:       "myapp",
+				},
+				sdk.Argument{
+					Type:        sdk.TextFieldInp,
+					Description: "Full image name including tag:",
+					Key:         "image-name",
+					Value:       "nginx:1.13",
+				},
+				sdk.Argument{
+					Type:        sdk.TextFieldInp,
+					Description: "Number of replicas:",
+					Key:         "replicas",
+					Value:       "1",
+				},
+			},
 		},
 		sdk.Job{
 			Handler:     CreateNamespace,
 			Title:       "Create namespace",
 			Description: "Create kubernetes namespace",
-			Priority:    10,
+			DependsOn:   []string{"Prepare Deployment"},
 		},
 		sdk.Job{
 			Handler:     CreateDeployment,
 			Title:       "Create deployment",
 			Description: "Create kubernetes app deployment",
-			Priority:    20,
+			DependsOn:   []string{"Create namespace"},
 		},
 		sdk.Job{
 			Handler:     CreateService,
 			Title:       "Create Service",
 			Description: "Create kubernetes service which exposes the service",
-			Priority:    30,
+			DependsOn:   []string{"Create deployment"},
 		},
 	}
 
--- main_test.go
+++ main_test.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	sdk "github.com/gaia-pipeline/gosdk"
 )
 
 func TestMain(m *testing.M) {
-	vaultAddress = "http://localhost:8200"
 	hostDNSName = "localhost"
-	err := GetSecretsFromVault()
+	err := GetSecretsFromVault(sdk.Arguments{})
 	if err != nil {
 		fmt.Printf("Cannot retrieve data from vault: %s\n", err.Error())
 		os.Exit(1)
 	}
 
-	err = CreateNamespace()
+	err = PrepareDeployment(createTestArguments())
+	if err != nil {
+		fmt.Printf("Cannot prepare the deployment: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	err = CreateNamespace(sdk.Arguments{})
 	if err != nil {
 		fmt.Printf("Cannot create namespace: %s\n", err.Error())
 		os.Exit(1)
@@ -26,15 +33,45 @@ func TestMain(m *testing.M) {
 }
 
 func TestCreateService(t *testing.T) {
-	err := CreateService()
+	err := CreateService(sdk.Arguments{})
 	if err != nil {
 		t.Error(err)
 	}
 }
 
 func TestCreateDeployment(t *testing.T) {
-	err := CreateDeployment()
+	err := CreateDeployment(sdk.Arguments{})
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func createTestArguments() sdk.Arguments {
+	return sdk.Arguments{
+		sdk.Argument{
+			Type:  sdk.VaultInp,
+			Key:   "vault-token",
+			Value: "123456789",
+		},
+		sdk.Argument{
+			Type:  sdk.VaultInp,
+			Key:   "vault-address",
+			Value: "http://localhost:8200",
+		},
+		sdk.Argument{
+			Type:  sdk.TextFieldInp,
+			Key:   "image-name",
+			Value: "nginx:1.13",
+		},
+		sdk.Argument{
+			Type:  sdk.TextFieldInp,
+			Key:   "app-name",
+			Value: "nginx",
+		},
+		sdk.Argument{
+			Type:  sdk.TextFieldInp,
+			Key:   "replicas",
+			Value: "1",
+		},
 	}
 }
